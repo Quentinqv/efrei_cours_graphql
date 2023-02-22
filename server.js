cd6625d7e6614b3d674bd0f7a09eacace8e8fb7c
Quentinqv <quentin010801@gmail.com>
2023-02-22 15:33:20 +0100

    🚀 RELEASE: Prisma bdd juste getter

--- server.js
+++ server.js
@@ -1,153 +1,95 @@
-const { buildSchema } = require("graphql");
-const { graphqlHTTP } = require("express-graphql");
-const express = require("express");
-const fs = require("fs");
-const data = require("./eleve2.json");
+const PrismaClient = require("@prisma/client").PrismaClient
+const express = require("express")
+const { graphqlHTTP } = require("express-graphql")
+const { makeExecutableSchema } = require("@graphql-tools/schema")
 
-let app = express();
+const prisma = new PrismaClient()
 
-let schema = buildSchema(`
-	type Eleve {
-		id: Int
-		lastname: String
-		firstname: String
-		classe: Classe
-	}
+const typeDefs = `
+type editors {
+  idEditors: Int
+  nameEditors: String
+  games: [games]
+}
 
-	type Classe {
-		id: Int
-		name: String
-		groupe: Int
-		promo: String
-		eleves: [Eleve]
-	}
+type games {
+  idGames: Int
+  nameGames: String
+  idEditors: Int
+  editors: editors
+  stock: [stock]
+}
 
-	type Query {
-		eleves: [Eleve]
-		classes: [Classe]
-	}
+type stores {
+  idStores: Int
+  nameStores: String
+  stock: [stock]
+}
 
-	type Mutation {
-		addEleve(lastname: String!, firstname: String!, classe: Int!): Eleve
-		updateEleve(id: Int!, lastname: String, firstname: String, classe: Int): Eleve
-		deleteEleve(id: Int!): Eleve
+type stock {
+  idStock: Int
+  idGames: Int
+  idStores: Int
+  units: Int
+  games: games
+  stores: stores
+}
 
-		addClasse(name: String!, groupe: Int!, promo: String!): Classe
-		updateClasse(id: Int!, name: String, groupe: Int, promo: String): Classe
-		deleteClasse(id: Int!): Classe
-	}
-`);
+type Query {
+  editors: [editors]
+  games: [games]
+  stores: [stores]
+  stock: [stock]
+}
+`
 
-let root = {
-  eleves: () => {
-	let out = []
-	data.eleves.forEach(eleve => {
-		let classe = data.classes.find(classe => classe.id === eleve.classe);
-		out.push({
-			id: eleve.id,
-			lastname: eleve.lastname,
-			firstname: eleve.firstname,
-			classe: classe
-		});
-	});
-    return out;
+const resolvers = {
+  Query: {
+    editors: async () => {
+      return await prisma.editors.findMany({
+        include: {
+          games: true
+        }
+      })
+    },
+    games: async () => {
+      return await prisma.games.findMany({
+        include: {
+          editors: true,
+          stock: true
+        }
+      })
+    },
+    stores: async () => {
+      return await prisma.stores.findMany({
+        include: {
+          stock: true
+        }
+      })
+    },
+    stock: async () => {
+      return await prisma.stock.findMany({
+        include: {
+          games: true,
+          stores: true
+        }
+      })
+    }
   },
-  addEleve: ({ lastname, firstname, classe }) => {
-    let maxId = 0;
-    data.eleves.forEach((eleve) => {
-      if (eleve.id > maxId) {
-        maxId = eleve.id;
-      }
-    });
-    maxId++;
+}
 
-    let eleve = {
-      id: maxId,
-      lastname,
-      firstname,
-      classe,
-    };
-    data.eleves.push(eleve);
-    return eleve;
-  },
-  updateEleve: ({ id, lastname, firstname, classe }) => {
-    let eleve = {
-      id,
-      lastname,
-      firstname,
-      classe,
-    };
-    let index = data.eleves.findIndex((eleve) => eleve.id === id);
-    data.eleves[index] = eleve;
-    return eleve;
-  },
-  deleteEleve: ({ id }) => {
-    let index = data.eleves.findIndex((eleve) => eleve.id === id);
-    let eleve = data.eleves[index];
-    data.eleves.splice(index, 1);
-    return eleve;
-  },
+const schema = makeExecutableSchema({
+  resolvers,
+  typeDefs,
+})
 
-  classes: () => {
-	let out = []
-	data.classes.forEach(classe => {
-		let eleves = data.eleves.filter(eleve => eleve.classe === classe.id);
-		out.push({
-			id: classe.id,
-			name: classe.name,
-			groupe: classe.groupe,
-			promo: classe.promo,
-			eleves: eleves
-		});
-	});
-	return out;
-  },
-  addClasse: ({ name, groupe, promo }) => {
-    let maxId = 0;
-    data.classes.forEach((classe) => {
-      if (classe.id > maxId) {
-        maxId = classe.id;
-      }
-    });
-    maxId++;
-
-    let classe = {
-      id: maxId,
-      name,
-      groupe,
-      promo,
-    };
-    data.classes.push(classe);
-    return classe;
-  },
-  updateClasse: ({ id, name, groupe, promo }) => {
-    let classe = {
-      id,
-      name,
-      groupe,
-      promo,
-    };
-    let index = data.classes.findIndex((classe) => classe.id === id);
-    data.classes[index] = classe;
-    return classe;
-  },
-  deleteClasse: ({ id }) => {
-    let index = data.classes.findIndex((classe) => classe.id === id);
-    let classe = data.classes[index];
-    data.classes.splice(index, 1);
-    return classe;
-  },
-};
-
+const app = express()
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
+    schema,
+    graphiql: true
   })
-);
+)
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
-});
+app.listen(4000)
